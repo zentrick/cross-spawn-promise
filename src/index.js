7fe1046331f0774308220c5c9642b8c73171a7a3
Tim De Pauw <tim.depauw@zentrick.com>
2016-03-03 12:11:46 +0100

    Keep stdout and stderr as buffers

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import crossSpawn from 'cross-spawn-async'
 
 export default (cmd, args, opt) => new Promise((resolve, reject) => {
   const proc = crossSpawn(cmd, args, opt)
-  let stdout = ''
-  let stderr = ''
-  proc.stdout.on('data', (data) => { stdout += data.toString() })
-  proc.stderr.on('data', (data) => { stderr += data.toString() })
+  const stdout = []
+  const stderr = []
+  proc.stdout.on('data', (data) => {
+    stdout.push(data)
+  })
+  proc.stderr.on('data', (data) => {
+    stderr.push(data)
+  })
   proc.once('close', (code) => {
     if (code !== 0) {
       const err = new Error(`Exited with status ${code}`)
-      err.stderr = stderr
+      err.stderr = Buffer.concat(stderr)
       reject(err)
     } else {
-      resolve(stdout)
+      resolve(Buffer.concat(stdout))
     }
   })
 })
